388a0f24ee0b466608107b37958881818a52b5a4
A6ad <asadjr29@gmail.com>
2025-04-26 15:48:09 +0600

    second push

--- armstrong_number.c
+++ armstrong_number.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<math.h>
 int is_armstrong(int num)
 {
     int original = num ,sum =0,digits =0;
@@ -13,10 +14,23 @@ int is_armstrong(int num)
     while (num!=0)
     {
         int digit= num % 10;
-        sum += pow()
+        sum += pow(digit,digits);
+        num /= 10;
     }
+
+    if(sum == original)
+    return sum;
 }
 int main() {
-    printf("%d",is_armstrong(153));
+    int number = 153;
+    if(is_armstrong(number))
+    {
+      printf("%d is armstrong number\n",number);
+    }
+    else 
+    {
+      printf("%d is not an armstrong number",number);
+    }
+
     return 0;
 }
